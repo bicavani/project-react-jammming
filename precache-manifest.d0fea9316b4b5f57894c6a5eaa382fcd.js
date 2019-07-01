0f0a7e9b8332c8d76636156bfe0fec4f90281c5c
Namlongcomputer <hoandm91@gmail.com>
2019-07-01 18:32:45 +0700

    Updates

---
+++ precache-manifest.d0fea9316b4b5f57894c6a5eaa382fcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fde606b553f703cc875647c09b3a2cc9",
+    "url": "/project-react-jammming/index.html"
+  },
+  {
+    "revision": "31faf53eb685440841c6",
+    "url": "/project-react-jammming/static/css/main.8f0895df.chunk.css"
+  },
+  {
+    "revision": "544c876ceda847331b13",
+    "url": "/project-react-jammming/static/js/2.b8a7688f.chunk.js"
+  },
+  {
+    "revision": "31faf53eb685440841c6",
+    "url": "/project-react-jammming/static/js/main.427813c8.chunk.js"
+  },
+  {
+    "revision": "e1c28b1fc0e16f829264",
+    "url": "/project-react-jammming/static/js/runtime~main.0f7f1f7b.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/project-react-jammming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
